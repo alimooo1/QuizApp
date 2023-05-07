dc4e1d5d991455b3cf5d6dc6fd42f8ae7689648e
Ali Moallemi <alimoallemi30@gmail.com>
2023-05-07 11:27:58 +0330

    QuizApp Class Added

--- dist/main.js
+++ dist/main.js
@@ -4,68 +4,84 @@ const answerBoxes = document.querySelectorAll(".answer");
 const results = document.querySelectorAll(".result .filler");
 const pageContent = document.querySelector(".answers");
 const questionBox = document.querySelector(".questionBox");
-const questionsList = await getQuestions();
-let currentLevel = 0;
-let answerIndex;
-function decode(str) {
-  let txt = new DOMParser().parseFromString(str, "text/html");
-  return txt.documentElement.textContent;
-}
-const showQuestion = (question) => {
-  const answersNumber = question.incorrect_answers.length;
-  if (answersNumber === 1) {
-    pageContent.children[2].classList.add("two-answer");
-    pageContent.children[3].classList.add("two-answer");
-  } else {
-    pageContent.children[2].classList.remove("two-answer");
-    pageContent.children[3].classList.remove("two-answer");
-  }
-  answerIndex = Math.floor(Math.random() * (answersNumber + 1));
-  let wrongAnswerIndex = 0;
-  questionLine.textContent = decode(question.question);
-  answerBoxes[answerIndex].textContent = decode(question.correct_answer);
-  for (let i = 0; i <= answersNumber; i++) {
-    if (i === answerIndex) {
-      continue;
-    } else {
-      answerBoxes[i].textContent = decode(
-        question.incorrect_answers[wrongAnswerIndex]
+class QizzApp {
+  constructor(_questionsList) {
+    this._questionsList = _questionsList;
+    this._currentLevel = 0;
+    this._answerIndex = 0;
+    this._decode = (str) => {
+      const txt = new DOMParser().parseFromString(str, "text/html");
+      return txt.documentElement.textContent;
+    };
+    this._showQuestion = (question) => {
+      let answersNumber = question.incorrect_answers.length;
+      if (answersNumber === 1) {
+        pageContent.children[2].classList.add("two-answer");
+        pageContent.children[3].classList.add("two-answer");
+      } else {
+        pageContent.children[2].classList.remove("two-answer");
+        pageContent.children[3].classList.remove("two-answer");
+      }
+      this._answerIndex = Math.floor(Math.random() * (answersNumber + 1));
+      let wrongAnswerIndex = 0;
+      questionLine.textContent = this._decode(question.question);
+      answerBoxes[this._answerIndex].textContent = this._decode(
+        question.correct_answer
       );
-      wrongAnswerIndex++;
-    }
+      for (let i = 0; i <= answersNumber; i++) {
+        if (i === this._answerIndex) {
+          continue;
+        } else {
+          answerBoxes[i].textContent = this._decode(
+            question.incorrect_answers[wrongAnswerIndex]
+          );
+          wrongAnswerIndex++;
+        }
+      }
+    };
+    this._checkAnswerHandler = (event) => {
+      const currentElement = event.target;
+      questionBox.classList.remove("old-question");
+      questionBox.classList.remove("new-question");
+      if (
+        currentElement.textContent ===
+        answerBoxes[this._answerIndex].textContent
+      ) {
+        answerBoxes[this._answerIndex].classList.add("correct-answer");
+        results[this._currentLevel].classList.add("correct-answer");
+        results[this._currentLevel].classList.add("showed-result");
+      } else {
+        answerBoxes[this._answerIndex].classList.toggle("correct-answer");
+        currentElement.classList.toggle("wrong-answer");
+        results[this._currentLevel].classList.add("showed-result");
+      }
+      this._currentLevel++;
+      setTimeout(() => {
+        questionBox.classList.add("old-question");
+      }, 1300);
+      setTimeout(() => {
+        if (this._currentLevel === 4) {
+          pageContent.textContent = "";
+          questionLine.textContent = "The Exam is finished.";
+        } else {
+          currentElement.classList.remove("wrong-answer");
+          answerBoxes[this._answerIndex].classList.remove("correct-answer");
+          this._showQuestion(this._questionsList[this._currentLevel]);
+        }
+        questionBox.classList.add("new-question");
+      }, 2100);
+    };
   }
-};
-const checkAnswerHandler = (event) => {
-  const currentElement = event.target;
-  questionBox.classList.remove("old-question");
-  questionBox.classList.remove("new-question");
-  if (currentElement.textContent === answerBoxes[answerIndex].textContent) {
-    answerBoxes[answerIndex].classList.add("correct-answer");
-    results[currentLevel].classList.add("correct-answer");
-    results[currentLevel].classList.add("showed-result");
-  } else {
-    answerBoxes[answerIndex].classList.toggle("correct-answer");
-    currentElement.classList.toggle("wrong-answer");
-    results[currentLevel].classList.add("showed-result");
+  _setCheckAnswerHandler() {
+    answerBoxes.forEach((answer) => {
+      answer.addEventListener("click", this._checkAnswerHandler);
+    });
   }
-  currentLevel++;
-  setTimeout(() => {
-    questionBox.classList.add("old-question");
-  }, 1300);
-  setTimeout(() => {
-    if (currentLevel === 4) {
-      pageContent.textContent = "";
-      questionLine.textContent = "The Exam is finished.";
-    } else {
-      currentElement.classList.remove("wrong-answer");
-      answerBoxes[answerIndex].classList.remove("correct-answer");
-      showQuestion(questionsList[currentLevel]);
-    }
-    questionBox.classList.add("new-question");
-  }, 2100);
-};
-answerBoxes.forEach((answer) => {
-  answer.addEventListener("click", checkAnswerHandler);
-});
-showQuestion(questionsList[currentLevel]);
+  Run() {
+    this._setCheckAnswerHandler();
+    this._showQuestion(this._questionsList[this._currentLevel]);
+  }
+}
+const myQuiz = new QizzApp(await getQuestions());
+myQuiz.Run();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -8,74 +8,90 @@ const results = document.querySelectorAll(
 ) as NodeListOf<HTMLDivElement>;
 const pageContent = document.querySelector(".answers") as HTMLDivElement;
 const questionBox = document.querySelector(".questionBox") as HTMLDivElement;
-const questionsList = (await getQuestions()) as QuestionsList[];
-let currentLevel = 0;
-let answerIndex: number;
 
-function decode(str: string) {
-  let txt = new DOMParser().parseFromString(str, "text/html");
-  return txt.documentElement.textContent as string;
+class QizzApp {
+  constructor(private _questionsList: QuestionsList[]) {}
+
+  private _currentLevel = 0;
+  private _answerIndex = 0;
+
+  private _decode = (str: string) => {
+    const txt = new DOMParser().parseFromString(str, "text/html");
+    return txt.documentElement.textContent as string;
+  };
+
+  private _showQuestion = (question: QuestionsList) => {
+    let answersNumber = question.incorrect_answers.length;
+    if (answersNumber === 1) {
+      pageContent.children[2].classList.add("two-answer");
+      pageContent.children[3].classList.add("two-answer");
+    } else {
+      pageContent.children[2].classList.remove("two-answer");
+      pageContent.children[3].classList.remove("two-answer");
+    }
+    this._answerIndex = Math.floor(Math.random() * (answersNumber + 1));
+    let wrongAnswerIndex = 0;
+    questionLine.textContent = this._decode(question.question);
+    answerBoxes[this._answerIndex].textContent = this._decode(
+      question.correct_answer
+    );
+    for (let i = 0; i <= answersNumber; i++) {
+      if (i === this._answerIndex) {
+        continue;
+      } else {
+        answerBoxes[i].textContent = this._decode(
+          question.incorrect_answers[wrongAnswerIndex]
+        );
+        wrongAnswerIndex++;
+      }
+    }
+  };
+
+  private _checkAnswerHandler = (event: MouseEvent) => {
+    const currentElement = event.target as HTMLDivElement;
+    questionBox.classList.remove("old-question");
+    questionBox.classList.remove("new-question");
+    if (
+      currentElement.textContent === answerBoxes[this._answerIndex].textContent
+    ) {
+      answerBoxes[this._answerIndex].classList.add("correct-answer");
+      results[this._currentLevel].classList.add("correct-answer");
+      results[this._currentLevel].classList.add("showed-result");
+    } else {
+      answerBoxes[this._answerIndex].classList.toggle("correct-answer");
+      currentElement.classList.toggle("wrong-answer");
+      results[this._currentLevel].classList.add("showed-result");
+    }
+    this._currentLevel++;
+
+    setTimeout(() => {
+      questionBox.classList.add("old-question");
+    }, 1300);
+
+    setTimeout(() => {
+      if (this._currentLevel === 4) {
+        pageContent.textContent = "";
+        questionLine.textContent = "The Exam is finished.";
+      } else {
+        currentElement.classList.remove("wrong-answer");
+        answerBoxes[this._answerIndex].classList.remove("correct-answer");
+        this._showQuestion(this._questionsList[this._currentLevel]);
+      }
+      questionBox.classList.add("new-question");
+    }, 2100);
+  };
+
+  private _setCheckAnswerHandler() {
+    answerBoxes.forEach((answer) => {
+      answer.addEventListener("click", this._checkAnswerHandler);
+    });
+  }
+
+  Run() {
+    this._setCheckAnswerHandler();
+    this._showQuestion(this._questionsList[this._currentLevel]);
+  }
 }
 
-const showQuestion = (question: QuestionsList) => {
-  const answersNumber = question.incorrect_answers.length;
-  if (answersNumber === 1) {
-    pageContent.children[2].classList.add("two-answer");
-    pageContent.children[3].classList.add("two-answer");
-  } else {
-    pageContent.children[2].classList.remove("two-answer");
-    pageContent.children[3].classList.remove("two-answer");
-  }
-  answerIndex = Math.floor(Math.random() * (answersNumber + 1));
-  let wrongAnswerIndex = 0;
-  questionLine.textContent = decode(question.question);
-  answerBoxes[answerIndex].textContent = decode(question.correct_answer);
-  for (let i = 0; i <= answersNumber; i++) {
-    if (i === answerIndex) {
-      continue;
-    } else {
-      answerBoxes[i].textContent = decode(
-        question.incorrect_answers[wrongAnswerIndex]
-      );
-      wrongAnswerIndex++;
-    }
-  }
-};
-
-const checkAnswerHandler = (event: MouseEvent) => {
-  const currentElement = event.target as HTMLDivElement;
-  questionBox.classList.remove("old-question");
-  questionBox.classList.remove("new-question");
-  if (currentElement.textContent === answerBoxes[answerIndex].textContent) {
-    answerBoxes[answerIndex].classList.add("correct-answer");
-    results[currentLevel].classList.add("correct-answer");
-    results[currentLevel].classList.add("showed-result");
-  } else {
-    answerBoxes[answerIndex].classList.toggle("correct-answer");
-    currentElement.classList.toggle("wrong-answer");
-    results[currentLevel].classList.add("showed-result");
-  }
-  currentLevel++;
-
-  setTimeout(() => {
-    questionBox.classList.add("old-question");
-  }, 1300);
-
-  setTimeout(() => {
-    if (currentLevel === 4) {
-      pageContent.textContent = "";
-      questionLine.textContent = "The Exam is finished.";
-    } else {
-      currentElement.classList.remove("wrong-answer");
-      answerBoxes[answerIndex].classList.remove("correct-answer");
-      showQuestion(questionsList[currentLevel]);
-    }
-    questionBox.classList.add("new-question");
-  }, 2100);
-};
-
-answerBoxes.forEach((answer) => {
-  answer.addEventListener("click", checkAnswerHandler);
-});
-
-showQuestion(questionsList[currentLevel]);
+const myQuiz = new QizzApp(await getQuestions());
+myQuiz.Run();
